36861b9982a7300f41a48510596916c2bf056fcb
B. Belchev <74198382+bdbelchev@users.noreply.github.com>
2021-05-04 12:01:13 +0300

    Задачи 3.5.21 + Материали към модул 3

---
+++ Модул 3 - Увод в ООП/1. Дефиниране на класове/01. PersonClass/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _01.PersonClass
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Person pesho = new Person();
+            pesho.name = "Pesho";
+            pesho.age = 20;
+
+            Person gosho = new Person();
+            gosho.name = "Gosho";
+            gosho.age = 18;
+
+            Person stamat = new Person();
+            stamat.name = "Stamat";
+            stamat.age = 43;
+        }
+    }
+}
---
+++ Модул 3 - Увод в ООП/1. Дефиниране на класове/03. Statistic/Person.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _03.Statistic
+{
+    class Person
+    {
+        string name;
+        int age;
+
+        public string Name
+        {
+            get { return this.name; }
+            set { this.name = value; }
+        }
+
+        public int Age
+        {
+            get { return this.age; }
+            set { this.age = value; }
+        }
+    }
+}
