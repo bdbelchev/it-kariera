b9a26afca2ac8ab1d14e868bbf3205dde7336f04
B. Belchev <74198382+bdbelchev@users.noreply.github.com>
2021-05-16 10:30:13 +0300

    Изпит УООП 2021

---
+++ Модул 3 - Увод в ООП/Изпит УООП 2021/Exam_Pharmacy/Medicine.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exam_Pharmacy
+{
+    class Medicine
+    {
+        private string name;
+        private double price;
+
+        public Medicine(string name, double price)
+        {
+            this.Name = name;
+            this.Price = price;
+        }
+
+        public string Name
+        {
+            get { return name; }
+            set { name = value; }
+        }
+
+        public double Price
+        {
+            get { return price; }
+            set
+            {
+                if (value < 0)
+                {
+                    throw new ArgumentException("Invalid price");
+                }
+
+                price = value;
+            }
+        }
+
+        public override string ToString()
+        {
+            return $"Medicine: {name} with price {price:f2}";
+        }
+    }
+}
---
+++ Модул 3 - Увод в ООП/Изпит УООП 2021/Exam_Pharmacy/Pharmacy.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exam_Pharmacy
+{
+    class Pharmacy
+    {
+        private string name;
+        private List<Medicine> medicines;
+
+        public Pharmacy(string name)
+        {
+            this.Name = name;
+            this.medicines = new List<Medicine>();
+        }
+
+        public string Name
+        {
+            get { return name; }
+            set
+            {
+                if (value.Length < 3)
+                {
+                    throw new ArgumentException("Invalid name");
+                }
+
+                name = value;
+            }
+        }
+
+        public void Order(Medicine medicine)
+        {
+            medicines.Add(medicine);
+        }
+
+        public bool Sell(Medicine medicine)
+        {
+            return medicines.Remove(medicine);
+        }
+
+        public double CalculateTotalPrice()
+        {
+            return medicines.Sum(m => m.Price);
+        }
+
+        public Medicine GetMedicineWithHighestPrice()
+        {
+            return medicines.OrderByDescending(m => m.Price).First();
+        }
+
+        public Medicine GetMedicineWithLowestPrice()
+        {
+            return medicines.OrderBy(m => m.Price).First();
+        }
+
+        public void RenamePharmacy(string newName)
+        {
+            this.Name = newName;
+        }
+
+        public void SellAllMedicines()
+        {
+            medicines.Clear();
+        }
+
+        public override string ToString()
+        {
+            StringBuilder sb = new StringBuilder();
+            sb.Append($"Pharmacy {name} has {medicines.Count} medicines and they are:")
+                .AppendLine();
+
+            sb.Append(medicines.Count < 1 ? "N/A" : string.Join(Environment.NewLine, medicines));
+
+            return sb.ToString();
+        }
+    }
+}
