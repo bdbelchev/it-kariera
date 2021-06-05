58c01a873b913e71e55494cf8f13e12651a2d341
B. Belchev <74198382+bdbelchev@users.noreply.github.com>
2021-06-05 16:07:30 +0300

    Опашка, стек

---
+++ Модул 4 - Увод в АСД/2. Линейни структури от данни/04. CircularQueue/CircularQueue.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _04.CircularQueue
+{
+    public class CircularQueue<T>
+    {
+        private const int DefaultCapacity = 16;
+
+        private T[] elements;
+        private int startIndex = 0;
+        private int endIndex = 0;
+
+        public int Count { get; private set; }
+
+        public CircularQueue(int capacity = DefaultCapacity)
+        {
+            elements = new T[capacity];
+        }
+
+        public void Enqueue(T element)
+        {
+            if (this.Count >= elements.Length)
+            {
+                this.Grow();
+            }
+
+            this.elements[endIndex] = element;
+            this.endIndex = (this.endIndex + 1) % this.elements.Length;
+            this.Count++;
+        }
+
+        private void Grow()
+        {
+            T[] newElements = new T[this.elements.Length * 2];
+            this.CopyAllElementsTo(newElements);
+            this.elements = newElements;
+            this.startIndex = 0;
+            this.endIndex = this.Count;
+        }
+
+        private void CopyAllElementsTo(T[] resultArr)
+        {
+            int sourceIndex = this.startIndex;
+            int destinationIndex = 0;
+
+            for (int i = 0; i < this.Count; i++)
+            {
+                resultArr[destinationIndex] = this.elements[sourceIndex];
+                sourceIndex = (sourceIndex + 1) % this.elements.Length;
+                destinationIndex++;
+            }
+        }
+
+        public T Dequeue()
+        {
+            if (this.Count==0)
+            {
+                throw new InvalidOperationException("The queue is empty!");
+            }
+
+            T result = this.elements[startIndex];
+            this.startIndex = (this.startIndex + 1) % this.elements.Length;
+            this.Count--;
+            return result;
+        }
+
+        public T[] ToArray()
+        {
+            T[] resultArray = new T[this.Count];
+            CopyAllElementsTo(resultArray);
+            return resultArray;
+        }
+    }
+}
---
+++ Модул 4 - Увод в АСД/2. Линейни структури от данни/05. ArrayStack/ArrayStack.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _05.ArrayStack
+{
+    public class ArrayStack<T>
+    {
+        private T[] elements;
+
+        public int Count { get; private set; }
+
+        private const int InitialCapacity = 16;
+
+        public ArrayStack(int capacity = InitialCapacity)
+        {
+            this.elements = new T[capacity];
+        }
+
+        public void Push(T element)
+        {
+            if (this.Count >= this.elements.Length)
+            {
+                this.Grow();
+            }
+
+            this.elements[this.Count++] = element;
+        }
+
+        public T Pop()
+        {
+            if (this.Count == 0)
+            {
+                throw new InvalidOperationException("Stack cannot be empty!");
+            }
+
+            return this.elements[this.Count--];
+        }
+
+        public T[] ToArray()
+        {
+            T[] returnArray = new T[this.Count];
+
+            for (int i = 0; i < this.Count; i++)
+            {
+                returnArray[i] = this.elements[i];
+            }
+
+            return returnArray;
+        }
+
+        private void Grow()
+        {
+            T[] newElements = new T[this.elements.Length * 2];
+
+            for (int i = 0; i < this.Count; i++)
+            {
+                newElements[i] = this.elements[i];
+            }
+
+            this.elements = newElements;
+        }
+    }
+}
