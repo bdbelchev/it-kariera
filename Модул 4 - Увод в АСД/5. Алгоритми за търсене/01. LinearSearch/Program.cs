a48b3c3b9cffb54b26e04a338b11a4dcee7f759f
B. Belchev <74198382+bdbelchev@users.noreply.github.com>
2021-07-01 11:31:52 +0300

    Алгоритми за търсене

---
+++ Модул 4 - Увод в АСД/5. Алгоритми за търсене/01. LinearSearch/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _01.LinearSearch
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] ints = Console.ReadLine().Split().Select(int.Parse).ToArray();
+
+            int search = int.Parse(Console.ReadLine());
+
+            Console.WriteLine(LinearSearch(ints, search));
+        }
+
+        static int LinearSearch<T>(T[] arr, T el)
+        {
+            for (int i = 0; i < arr.Length; i++)
+            {
+                if (Equals(arr[i], el))
+                {
+                    return i;
+                }
+            }
+
+            return -1;
+        }
+    }
+}
---
+++ Модул 4 - Увод в АСД/5. Алгоритми за търсене/02. BinarySearch/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _02.BinarySearch
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] ints = Console.ReadLine().Split().Select(int.Parse).ToArray();
+
+            int search = int.Parse(Console.ReadLine());
+
+            Console.WriteLine(BinarySearch(ints, search, 0, ints.Length));
+        }
+
+        static int BinarySearch(int[] arr, int el, int start, int end)
+        {
+            int mid = (start + end) / 2;
+
+            if (arr[mid] > el)
+            {
+                return BinarySearch(arr, el, start, mid - 1);
+            }
+
+            if (arr[mid] < el)
+            {
+                return BinarySearch(arr, el, mid + 1, end);
+            }
+
+            return mid;
+        }
+    }
+}
