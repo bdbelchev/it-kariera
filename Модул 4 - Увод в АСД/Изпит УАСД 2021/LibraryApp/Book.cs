ded62e684c711e90e12b9630da2f0a5b0d88c8b2
B. Belchev <74198382+bdbelchev@users.noreply.github.com>
2021-07-04 11:48:27 +0300

    Изпит УАСД 2021

---
+++ Модул 4 - Увод в АСД/Изпит УАСД 2021/LibraryApp/Book.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LibraryApp
+{
+    class Book
+    {
+        private string id;
+        private Book next;
+
+        public Book(string bookId)
+        {
+            id = bookId;
+        }
+
+        public string BookId
+        {
+            get { return id; }
+        }
+
+        public Book Next
+        {
+            get { return next; }
+            set { next = value; }
+        }
+
+        public override string ToString()
+        {
+            return $"Book: {id}";
+        }
+    }
+}
---
+++ Модул 4 - Увод в АСД/Изпит УАСД 2021/LibraryApp/Shelf.cs
@@ -0,0 +1,167 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LibraryApp
+{
+    class Shelf
+    {
+        private Book head;
+        private Book tail;
+        private int count;
+
+        public Shelf()
+        {
+            Count = 0;
+        }
+
+        public int Count
+        {
+            get { return count; }
+            set { count = value; }
+        }
+
+        public void AddBook(string bookId)
+        {
+            Book newBook = new Book(bookId);
+
+            if (this.Count == 0)
+            {
+                head = tail = newBook;
+            }
+            else
+            {
+                tail.Next = newBook;
+                tail = newBook;
+            }
+
+            Count++;
+        }
+
+        public void AddSpecialBook(string bookId)
+        {
+            Book newBook = new Book(bookId);
+
+            if (this.Count == 0)
+            {
+                head = tail = newBook;
+            }
+            else
+            {
+                Book oldHead = head;
+                head = newBook;
+                head.Next = oldHead;
+            }
+
+            Count++;
+        }
+
+        public Book CheckBookIsPresent(string bookId)
+        {
+            if (Count == 0)
+            {
+                return null;
+            }
+
+            Book currentBook = head;
+
+            while (currentBook.BookId != bookId)
+            {
+                if (currentBook.Next == null)
+                {
+                    return null;
+                }
+
+                currentBook = currentBook.Next;
+            }
+
+            return currentBook;
+        }
+
+        public bool ReleaseBook(string bookId)
+        {
+            if (CheckBookIsPresent(bookId) == null)
+            {
+                return false;
+            }
+
+            if (head.BookId == bookId)
+            {
+                head = head.Next;
+                Count--;
+                return true;
+            }
+
+            Book currentBook = head;
+
+            while (currentBook.Next.BookId != bookId)
+            {
+                currentBook = currentBook.Next;
+            }
+
+            if (currentBook.Next == tail)
+            {
+                tail = currentBook;
+            }
+
+            currentBook.Next = currentBook.Next.Next;
+            Count--;
+            return true;
+        }
+
+        public bool ReleaseBook(int index)
+        {
+            if (index < 0 || index > Count - 1)
+            {
+                return false;
+            }
+
+            Book currentBook = head;
+
+            if (index == 0)
+            {
+                head = head.Next;
+                Count--;
+                return true;
+            }
+
+            for (int i = 0; i < index - 1; i++)
+            {
+                currentBook = currentBook.Next;
+            }
+
+            if (currentBook.Next == tail)
+            {
+                tail = currentBook;
+            }
+
+            currentBook.Next = currentBook.Next.Next;
+            Count--;
+            return true;
+        }
+
+        public StringBuilder ShelfInformation()
+        {
+            StringBuilder sb = new StringBuilder();
+
+            if (Count == 0)
+            {
+                sb.Append("Shelf is empty!");
+            }
+            else
+            {
+                Book currentBook = head;
+
+                while (currentBook != null)
+                {
+                    sb.Append(currentBook).AppendLine();
+                    currentBook = currentBook.Next;
+                }
+            }
+
+            return sb;
+        }
+    }
+}
