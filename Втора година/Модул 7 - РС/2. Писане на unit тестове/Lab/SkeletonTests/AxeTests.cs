04755f48fd80e244c62047b681dadfddea7ac915
B. Belchev <74198382+bdbelchev@users.noreply.github.com>
2022-02-10 17:12:06 +0200

    Задачи от „Писане на unit тестове“

---
+++ Втора година/Модул 7 - РС/2. Писане на unit тестове/Lab/SkeletonTests/AxeTests.cs
@@ -0,0 +1,39 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SkeletonTests
+{
+    [TestFixture]
+    public class AxeTests
+    {
+        [Test]
+        public void AxeLosesDurabilityAfterAttack()
+        {
+            Axe axe = new Axe(10, 10);
+            Dummy dummy = new Dummy(10, 10);
+
+            axe.Attack(dummy);
+
+            Assert.AreEqual(9, axe.DurabilityPoints, "Axe durability doesn't change after attack");
+        }
+
+        [Test]
+        public void BrokenAxeCannotAttack()
+        {
+            Axe axe = new Axe(2,2);
+            Dummy dummy = new Dummy(20,20);
+
+            axe.Attack(dummy);
+            axe.Attack(dummy);
+
+            var ex = Assert.Throws<InvalidOperationException>(() => axe.Attack(dummy));
+
+            Assert.That(ex.Message, Is.EqualTo("Axe is broken."));
+        }
+    }
+}
---
+++ Втора година/Модул 7 - РС/2. Писане на unit тестове/Lab/SkeletonTests/DummyTests.cs
@@ -0,0 +1,59 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SkeletonTests
+{
+    [TestFixture]
+    public class DummyTests
+    {
+        [Test]
+        public void DummyLosesHealthIfAttacked()
+        {
+            Dummy dummy = new Dummy(10, 10);
+            Axe axe = new Axe(5,5);
+
+            axe.Attack(dummy);
+
+            Assert.AreEqual(5, dummy.Health, "Dummy does not lose health points when attacked.");
+        }
+
+        [Test]
+        public void DeadDummyThrowsExceptionIfAttacked()
+        {
+            Dummy dummy = new Dummy(5,5);
+            Axe axe = new Axe(10,10);
+
+            axe.Attack(dummy);
+
+            Assert.Throws<InvalidOperationException>(() => axe.Attack(dummy), "Dead dummy does not throw exception when attacked.");
+        }
+
+        [Test]
+        public void DeadDummyGivesExp()
+        {
+            Dummy dummy = new Dummy(5, 10);
+            Hero hero = new Hero("Test");
+
+            hero.Attack(dummy);
+
+            Assert.AreEqual(10, hero.Experience, "Dead dummy does not give experience!");
+        }
+
+        [Test]
+        public void AliveDummyThrowsExceptionWhenGivingExp()
+        {
+            Dummy dummy = new Dummy(20, 20);
+            Hero hero = new Hero("Test");
+
+            hero.Attack(dummy);
+
+            Assert.Throws<InvalidOperationException>((() => dummy.GiveExperience()), "Living dummy does not throw exception when attempting to give experience.");
+        }
+    }
+}
