4b22575c28c07f73ac6ecf03e2d49a0ac3400947
B. Belchev <74198382+bdbelchev@users.noreply.github.com>
2022-10-29 17:08:32 +0300

    Добавяне на други примерни задачи за изпит

---
+++ Трета година/Модул 9 - АСД/FoodVouchers/Program.cs
@@ -0,0 +1,31 @@
+﻿namespace FoodVouchers
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int days = int.Parse(Console.ReadLine());
+
+            int neededValue = days * 20;
+            int currentValue = 0;
+            int count = 0;
+
+            while (currentValue < neededValue)
+            {
+                if (currentValue + 100 > neededValue)
+                {
+                    currentValue += 20;
+                    count++;
+
+                }
+                else
+                {
+                    currentValue += 100;
+                    count++;
+                }
+            }
+
+            Console.WriteLine(count);
+        }
+    }
+}
---
+++ Трета година/Модул 9 - АСД/PositiveMultiplication/Program.cs
@@ -0,0 +1,22 @@
+﻿namespace PositiveMultiplication
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] numbers = Console.ReadLine().Split(" ").Select(int.Parse).ToArray();
+
+            Console.WriteLine(Multiply(numbers[0], numbers[1]));
+        }
+
+        private static int Multiply(int n1, int n2)
+        {
+            if (n2 == 1)
+            {
+                return n1;
+            }
+
+            return Multiply(n1, n2 - 1) + n1;
+        }
+    }
+}
